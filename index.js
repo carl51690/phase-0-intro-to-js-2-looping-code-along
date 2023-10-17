55d41675c01f087cd94a6eb92321807adeb081ea
Carl Adams <carl51690@gmail.com>
2023-10-16 20:45:42 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(Cards){
+  const message = []
+  for (let a = 0; a < Cards.length; a++){
+    message.push(`Thank you, ${Cards[a]}, for the wonderful surprise gift!`);
+  }
+  return message
+}
+
+function countDown(num){
+while (num >= 0) {
+  console.log(num);
+  num--;
+}
+}
+
